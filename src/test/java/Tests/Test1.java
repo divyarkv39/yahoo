de4b7b84ea94536b6eb2ba0a3a9c649980ec5416
divya <divya@gmail.com>
2022-04-14 22:24:09 +0530

    commitchanges

--- src/test/java/Tests/Test1.java
+++ src/test/java/Tests/Test1.java
@@ -2,8 +2,13 @@ package Tests;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		String x="divya";
+		String y="pavan";
+		int len=x.length()+y.length();
+		String str[]=new String[20];
+			System.out.println(x+y);
 
 	}
 
